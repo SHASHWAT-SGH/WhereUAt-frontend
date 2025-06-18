f596061adea9e7a3ad924eac9675b9e73ed6d4c8
SHASHWAT-SGH <vnsshashwat@gmail.com>
2025-06-18 09:27:41 +0530

    created track page

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -31,15 +31,17 @@ export default function TabsLayout() {
           ),
         }}
       />
+
       <Tabs.Screen
-        name="events"
+        name="track"
         options={{
-          title: "Events",
+          title: "Track",
           tabBarIcon: ({ color, size }) => (
-            <Ionicons name="calendar-outline" size={size} color={color} />
+            <Ionicons name="location-outline" size={size} color={color} />
           ),
         }}
       />
+
       <Tabs.Screen
         name="profile"
         options={{
